623e9dbf608573375dca0e2e65600c520dd19893
adrianestigarribia <estigarribiaadrian13@gmail.com>
2026-06-01 20:34:11 -0300

    Update index.php

--- src/views/index.php
+++ src/views/index.php
@@ -1,4 +1,140 @@
 <main>
-    <h1>✅ 200 OK</h1>
-    <p>Si ves esto, el servidor de Slim y el motor de plantillas estan funcionando correctamente.</p>
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <style>  
+            .fondo{
+            background: linear-gradient(to bottom, #1e3c72, #2a5298);
+        }
+        </style>
+
+        <style>
+        .content {padding: 2em 0.5em; text-align: center;}
+        .s1 {background-color: #ca9f77;}
+        .s2 {background-color: #cbc168;}
+        .s3 {background-color: #a3c55b;}
+        .s4 {background-color: #76d179;}
+        .s5 {background-color: #85e3c1;}
+        .s6 {background-color: #79bfe5;}
+    </style>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>SetupGYM_netK</title>
+    </head>
+
+    <body class="fondo">
+    <header>
+        <nav class="">
+       <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+        </li>
+      </ul>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+ </nav>
+
+        <h1 class="text-center">CodeGYM Net</h1>
+        <nav  class="p-3 mb-2 bg-primary text-white" >
+<p> <img src="ima2.avif" alt=""> holaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
+        </nav>
+    </header>
+
+    <nav>
+        <!-- Menú de navegación -->
+    </nav>
+
+    <main>
+        <!-- Contenido principal -->
+    </main>
+
+    <aside>
+        <!-- Información secundaria -->
+    </aside>
+
+    <footer>
+      <section>
+         <div class="container-sm g-0">
+        <div class="row g-0">
+            <div class="col-3 ">
+                <div class="content s1"><h4>logo</h4></div>
+            </div>
+            <div class="col-1 content s2">
+                <div class="content s2"><h4>contacto</h4></div>
+                <ul>  
+                  <li>
+                      <h4><a href="contact">contactanos</a></h4>
+                  </li>
+                </ul>
+            </div>
+            <div class="col-1 content s3">
+                <div class="content s3">acerca de nosotros</div>
+                <ul>  
+                  <li>
+                      <h4><a href="info">quienes somos</a></h4>
+                  </li>
+                </ul>
+            </div>
+            <div class="col-1 content s4">
+                <div class="content s4">inicio</div>
+                <ul>  
+                  <li>
+                      <h4><a href="sing in">inicio de sesion</a></h4>
+                  </li>
+                </ul>
+            </div>
+            <div class="col-1 content s4">
+                <div class="content s4">telefonos</div>
+                <ul>  
+                  <li>
+                      <h4><a href="telefono">Comunicate</a></h4>
+                  </li>
+                </ul>
+            </div>
+            <div class="col-1 content s6">
+                <div class="content s6">horarios</div>
+                <ul>  
+                  <li>
+                      <h4><a href="atencion">horarios de atencion</a></h4>
+                  </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+      </section>
+        <!-- Pie de página -->
+    </footer>
+
+    </body>
+    </html>
 </main>
+
