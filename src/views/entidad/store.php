d98bc8cde3044d19533dcbbbe35a974737ea6fb5
adrianestigarribia <estigarribiaadrian13@gmail.com>
2026-06-17 19:09:22 -0300

    Update store.php

--- src/views/entidad/store.php
+++ src/views/entidad/store.php
@@ -1,4 +1,19 @@
 <main>
     <h1>✅ 200 OK</h1>
     <p>Si ves esto, el servidor de Slim y el motor de plantillas estan funcionando correctamente.</p>
+<main>
+    <h1>Creando Producto</h1>
+    <form action="/entidad" method="POST">
+        <label for="nombre"><b>Nombre:</b></label><br>
+        <input type="text" name="nombre" placeholder="Nombre del producto"><br><br>
+
+        <label for="precio"><b>Precio:</b></label><br>
+        <input type="text" name="precio" placeholder="Precio"><br><br>
+
+        <label for="descripcion"><b>Descripción:</b></label><br>
+        <textarea name="descripcion" placeholder="Descripción del producto"></textarea><br><br>
+
+        <input type="submit" value="Crear Producto">
+    </form>
+</main>
 </main>
