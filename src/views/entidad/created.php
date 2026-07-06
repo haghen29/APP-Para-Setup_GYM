45b39e981b440d342e426e53d0575e6a63d71b1b
adrianestigarribia <estigarribiaadrian13@gmail.com>
2026-07-06 13:07:52 -0300

    Delete src/views/entidad/created.php

--- src/views/entidad/created.php
+++
@@ -1,6 +0,0 @@
-<main>
-    <h1>✅ Producto Creado</h1>
-    <p><b>Nombre:</b> <?= ($nombre) ?></p>
-    <p><b>Precio:</b> <?= ($precio) ?></p>
-    <p><b>Descripción:</b> <?= ($descripcion) ?></p>
-</main>
